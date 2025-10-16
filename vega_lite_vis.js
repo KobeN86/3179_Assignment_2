ea7f36d7eb16d304eda5ecb0be5c6cf559f64afd
KobeN86 <knad0003@student.monash.edu>
2025-10-17 10:17:16 +1100

    Move shit around

--- vega_lite_vis.js
+++
@@ -1,6 +0,0 @@
-var vg_1 = "daily_cost_bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-    console.log('Chart loaded successfully!');
-}).catch(function(error) {
-    console.error('Error loading chart:', error);
-});
